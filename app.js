3e75bd5fc2c541aa827b9747b9f0544af9620f7a
allengblack <allengblack@gmail.com>
2016-08-24 11:26:19 +0100

    Created quiz_list JSON file.

--- app.js
+++ app.js
@@ -7,10 +7,24 @@ function errorHandler (err, obj) {
 var app = angular.module("quiz-app", [ui.bootstrap]);
 
 app.controller("quiz-ctrl", function ($scope, $sce, $http) {
+    $scope.quiz_list = [];
 
-    $http.get("bc-19-quiz-application/quiz_lib/GoT-Quiz.json").success(function (response) {
+    $http.get("quiz_lib/GoT-Quiz.json").success(function (response) {
         $scope.quiz = response;
         console.log(response);
     }).error(errorHandler);
 
+    $http.get("quiz_lib/quiz-choices.json").success(function (response) {
+        $scope.quiz_list = response;
+        console.log(response);
+    }).error(errorHandler);
+
 });
+
+app.controller("quiz-choices", function ($scope, $sce, $http) {
+
+
+
+});
+
+// $scope.quiz-choices = []

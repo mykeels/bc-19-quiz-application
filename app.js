943314a8bf855950f918469a2253e43f7fe32055
allengblack <allengblack@gmail.com>
2016-08-24 10:54:21 +0100

    Writing controller for landing page.

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-/**
- * Created by g-man on 8/24/16.
- */
-// var angular = require('angular');
 
-angular.module('quiz-app', ['ui.bootstrap']);
+function errorHandler (err, obj) {
+    //a global error handler
+    console.error(err, obj);
+}
+
+var app = angular.module("quiz-app", [ui.bootstrap]);
+
+app.controller("quiz-ctrl", function ($scope, $sce, $http) {
+
+    $http.get("bc-19-quiz-application/quiz_lib/GoT-Quiz.json").success(function (response) {
+        $scope.quiz = response;
+        console.log(response);
+    }).error(errorHandler);
+
+});
